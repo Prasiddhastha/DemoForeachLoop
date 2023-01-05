7ddaa11df96b0a948c2dcac82683ed47610c827d
zenoBeast <zenoBeast@WIN-V9I6Q1UBA8C>
2023-01-05 14:12:33 +0545

    Add project files.

---
+++ DemoForeachLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace CsharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //forloop
+            var name = "Prasiddha Shrestha";
+            for(var i=0;i<name.Length;i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+            Console.WriteLine("\n");
+            //foreachloop
+            foreach(var character in name)
+            {
+                Console.WriteLine(character);
+            }
+        }
+    }
+}
